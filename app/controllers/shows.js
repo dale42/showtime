689c57fde6d41d3e3f1eb7abc1f6794d2c11568e
Dale McGladdery <dale@group42.ca>
2017-08-19 21:38:05 -0700

    Add input validation to "Add show"

--- app/controllers/shows.js
+++ app/controllers/shows.js
@@ -3,4 +3,5 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   sortProperties: ['name:asc'],
   showList: Ember.computed.sort('model', 'sortProperties'),
+  isAddShowDisabled: Ember.computed.empty('name')
 });
