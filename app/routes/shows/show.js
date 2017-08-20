e1c8bf80897de31ac75d58e157c2a68d22ed07e4
Dale McGladdery <dale@group42.ca>
2017-08-20 15:08:25 -0700

    Add ability to add and delete show elements

--- app/routes/shows/show.js
+++ app/routes/shows/show.js
@@ -5,4 +5,27 @@ export default Ember.Route.extend({
     return this.store.findRecord('show', params.id);
   },
 
+  actions: {
+    addElement(controller) {
+      const show = controller.get('model');
+
+      let newElement = this.store.createRecord('element', {
+        name: controller.get('name'),
+        length: controller.get('length'),
+        show: show
+      });
+      // show.get('items').pushObject(newItem);
+      return newElement.save().then(function () {
+        show.save();
+        controller.set('name', '');
+        controller.set('length', '');
+      });
+    },
+
+    deleteItem(item) {
+      return item.destroyRecord();
+    }
+
+  }
+
 });
