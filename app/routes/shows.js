e177b20b0758960ff8452d60d96c111d760b0fa8
Dale McGladdery <dale@group42.ca>
2017-08-19 20:52:44 -0700

    Add show display list and ability to add/delete shows

--- app/routes/shows.js
+++ app/routes/shows.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('show');
+  },
+
+  actions: {
+    addShow() {
+      let controller = this.get('controller');
+
+      let newShow = this.store.createRecord('show', {
+        name: controller.get('name'),
+      });
+      return newShow.save().then(function () {
+        controller.set('name', '');
+      });
+    },
+
+    deleteShow(show) {
+      return show.destroyRecord();
+    }
+
+  }
+
 });
