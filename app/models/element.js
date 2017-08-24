19026838119d4a764606a5d8902044612e689946
Dale McGladdery <dale@group42.ca>
2017-08-24 16:24:03 -0700

    Remove position attribute from element model

--- app/models/element.js
+++ app/models/element.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   show:     DS.belongsTo('show'),
-  position: DS.attr('number'),
   name:     DS.attr('string'),
   length:   DS.attr('number'),  // in seconds
 });
