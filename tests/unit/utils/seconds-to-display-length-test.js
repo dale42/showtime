1ebaa9f954782e1f491997fa40bbb2b0a55d6098
Dale McGladdery <dale@group42.ca>
2017-08-29 12:52:01 -0700

    Add util function to format seconds into time display

---
+++ tests/unit/utils/seconds-to-display-length-test.js
@@ -0,0 +1,10 @@
+import secondsToDisplayLength from 'showtime/utils/seconds-to-display-length';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | seconds to display length');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = secondsToDisplayLength();
+  assert.ok(result);
+});
