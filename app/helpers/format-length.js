4b04d417b30b1a2840862197d0b1e9551964617b
Dale McGladdery <dale@group42.ca>
2017-08-29 12:53:27 -0700

    Change format-length helper to use secondsToDisplayLength util function

--- app/helpers/format-length.js
+++ app/helpers/format-length.js
@@ -1,29 +1,8 @@
 import Ember from 'ember';
+import secondsToDisplayLength from '../utils/seconds-to-display-length';
 
 export function formatLength(length) {
-  if (length == 0) {
-    return '---';
-  }
-  let hms = [
-    { unitName: 'h', value: Math.floor(length / 3600) },
-    { unitName: 'm', value: Math.floor((length / 60) % 60) },
-    { unitName: 's', value: length % 60 }
-  ];
-
-  let displayTime = hms.reduce((display, unit) => {
-    // Don't display 0unit unless a higher unit is present
-    if (unit.value == 0 && display == '') {
-      return display;
-    }
-    let unitDisplay = unit.value.toString() + unit.unitName;
-    if (display != '') {
-      unitDisplay = ' ' + unitDisplay;
-    }
-
-    return display + unitDisplay;
-  }, '');
-
-  return displayTime;
+  return secondsToDisplayLength(length);
 }
 
 export default Ember.Helper.helper(formatLength);
