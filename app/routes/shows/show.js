1c74dbe94ea0436cb612ebb97732d51dded03925
Dale McGladdery <dale@group42.ca>
2017-08-29 12:59:41 -0700

    Add abilty to save a show into a text file

--- app/routes/shows/show.js
+++ app/routes/shows/show.js
@@ -1,4 +1,8 @@
+// app/routes/shows/show.js
+
 import Ember from 'ember';
+import secondsToDisplayLength from '../../utils/seconds-to-display-length';
+import FileSaver from "npm:file-saver";
 
 export default Ember.Route.extend({
   model(params) {
@@ -6,7 +10,7 @@ export default Ember.Route.extend({
   },
 
   actions: {
-    // deleteElement action is on controller because of modal dialog
+    // NOTE: deleteElement action is on controller because of modal dialog
 
     addElement(controller) {
       const show = controller.get('model');
@@ -28,6 +32,29 @@ export default Ember.Route.extend({
     reorderElements(groupModel, elementModels) {
       groupModel.set('elements', elementModels);
       groupModel.save();
+    },
+
+    saveAsText() {
+      const show = this.controller.get('model');
+      const showName = show.get('name');
+      const longestNameLength = show.get('elements').reduce((longestLength, element) => {
+        const nameLength = element.get('name').length;
+        return (nameLength > longestLength) ? nameLength : longestLength;
+      }, 0);
+
+      let output = [];
+      output.push(`${showName}\r\n`);
+      output.push('='.repeat(showName.length) + '\r\n\r\n');
+      output.push('Run time: ' + secondsToDisplayLength(this.controller.get('totalTime')) + '\r\n\n');
+      show.get('elements').forEach(element => {
+        const elementDisplayWidth = longestNameLength + 2;
+        const displayElement = element.get('name') + ' '.repeat(elementDisplayWidth - element.get('name').length);
+        const displayLength = secondsToDisplayLength(element.get('length'));
+        output.push(`- ${displayElement}${displayLength}\r\n`);
+      });
+
+      let blob = new Blob(output, {type: "text/plain;charset=utf-8"});
+      FileSaver.saveAs(blob, `${showName}.txt`);
     }
 
   }
