965bfce1ea1edd7743fa8f867aa138b579264279
Dale McGladdery <dale@group42.ca>
2017-09-04 17:32:06 -0700

    Add modal dialog for creating show

--- app/controllers/shows/index.js
+++ app/controllers/shows/index.js
@@ -11,13 +11,61 @@ export default Ember.Controller.extend({
 
   showToDelete: null,
 
+  showToEdit: null,
+
+  displayShowForm: false,
+
+  inputNameValue: '',
+
+  showFormButtonText: '',
+
   actions: {
     deleteShow(show) {
       return show.destroyRecord()
         .then(() => {
           this.set('showToDelete', null);
         });
-    }
+    },
+
+    openShowForm(show) {
+      if (show === null) {
+        this.set('showFormButtonText', 'Create');
+        this.set('showToEdit', null);
+      } else {
+        this.set('showFormButtonText', 'Save');
+        this.set('showToEdit', show);
+      }
+      this.set('displayShowForm', true);
+    },
+
+    populateShowForm() {
+      const show = this.get('showToEdit');
+      if (show === null) {
+        this.set('inputNameValue', '');
+      } else {
+        this.set('inputNameValue', show.get('name'));
+      }
+    },
+
+    saveShow(form) {
+      const controller = this;
+      let doTransition = false;
+      this.set('displayShowForm', false);
+      let show = this.get('showToEdit');
+      if (show === null) {
+        show = this.store.createRecord('show');
+        doTransition = true;
+      }
+
+      show.set('name', form.inputNameValue);
+
+      return show.save().then(function () {
+        if (doTransition) {
+          controller.transitionToRoute('shows.show', show);
+        }
+      });
+    },
+
   }
 
 });
