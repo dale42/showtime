06a2a479f65b3cc544bafe88fd08d9d618564127
Dale McGladdery <dale@group42.ca>
2017-09-08 15:54:07 -0700

    Add format display options, and "colon" display format

--- app/utils/seconds-to-display-length.js
+++ app/utils/seconds-to-display-length.js
@@ -1,4 +1,4 @@
-export default function secondsToDisplayLength(lengthInSeconds) {
+export default function secondsToDisplayLength(lengthInSeconds, format) {
   if (isNaN(lengthInSeconds) || lengthInSeconds === 0) {
     return '---';
   }
@@ -7,19 +7,29 @@ export default function secondsToDisplayLength(lengthInSeconds) {
     { unitName: 'm', value: Math.floor((lengthInSeconds / 60) % 60) },
     { unitName: 's', value: lengthInSeconds % 60 }
   ];
+  let displayTime = '';
 
-  let displayTime = hms.reduce((display, unit) => {
-    // Don't display a 0unit (eg: 0m) unless a higher unit is already displayed
-    if (unit.value == 0 && display == '') {
-      return display;
-    }
-    let unitDisplay = unit.value.toString() + unit.unitName;
-    if (display != '') {
-      unitDisplay = ' ' + unitDisplay;
-    }
+  if (format === 'colon') {
+    displayTime = hms.reduce((display, unit) => {
+      if (display !== '') {
+        display = display + ':';
+      }
+      return display = display + ('0' + unit.value.toString()).substr(-2);
+    }, '');
+  } else {
+    displayTime = hms.reduce((display, unit) => {
+      // Don't display a 0unit (eg: 0m) unless a higher unit is already displayed
+      if (unit.value == 0 && display == '') {
+        return display;
+      }
+      let unitDisplay = unit.value.toString() + unit.unitName;
+      if (display != '') {
+        unitDisplay = ' ' + unitDisplay;
+      }
 
-    return display + unitDisplay;
-  }, '');
+      return display + unitDisplay;
+    }, '');
+  }
 
   return displayTime;
 }
