e01bcf46ddb1b9495d1c351052ea6caa50e8d13d
Dale McGladdery <dale@group42.ca>
2017-09-13 13:47:10 -0700

    Update format-length helper to use moments.duration

--- app/helpers/format-length.js
+++ app/helpers/format-length.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
-import secondsToDisplayLength from '../utils/seconds-to-display-length';
+import moment from 'moment';
 
-export function formatLength([length, ...rest]) {
-  const format = rest[0] || '';
-  return secondsToDisplayLength(length, format);
+export function formatLength([length]) {
+  if (!length) return '';
+  return moment.duration(length, 'seconds').format('h[h] m[m] s[s]')
+    .replace(' 0m 0s', ' ').replace(' 0s', ' ');
 }
 
 export default Ember.Helper.helper(formatLength);
