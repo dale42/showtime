7f15282c3a26bfd09fa7787e0cbd62b998ff8b26
Dale McGladdery <dale@group42.ca>
2017-09-20 12:04:54 -0700

    Remove unused action from routes/shows

--- app/routes/shows.js
+++ app/routes/shows.js
@@ -7,21 +7,4 @@ export default Ember.Route.extend({
     return this.store.findAll('show');
   },
 
-  actions: {
-    addShow(controller) {
-      const router = this.get('router');
-
-      let newShow = this.store.createRecord('show', {
-        name: controller.get('name'),
-      });
-      return newShow.save().then(function () {
-        controller.set('name', '');
-        router.transitionTo('shows.show', newShow);
-      });
-    },
-
-    // deleteShow action is on controller/shows/index because of modal dialog
-
-  }
-
 });
